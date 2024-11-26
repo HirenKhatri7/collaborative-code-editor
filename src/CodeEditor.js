5fc1e2f983432ac0f9f0fe68ce57e43c96631761
HirenKhatri7 <khatrih879@gmail.com>
2024-11-27 01:47:09 +0530

    env variable set

--- src/CodeEditor.js
+++ src/CodeEditor.js
@@ -15,7 +15,7 @@ import axios from 'axios';
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   };
   
 
